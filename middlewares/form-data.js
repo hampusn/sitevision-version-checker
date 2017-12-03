1eaf31c0f0f3061210fa9fb609a002c25f0e36b8
Hampus Nordin <nordin.hampus@gmail.com>
2017-12-03 15:51:48 +0100

    Removed dependency 'valid-url' and added redirect to remove everything but the origin from requested URL.

--- middlewares/form-data.js
+++ middlewares/form-data.js
@@ -1,14 +1,35 @@
 // Add form data to request
-const validUrl = require('valid-url');
+const URL = require('url').URL;
+
+function ensureProtocol (url) {
+  if (url.substr(0, 4) !== 'http') {
+    url = 'http://' + url;
+  }
+  return url;
+}
 
 module.exports = () => {
   return function formDataMiddleware (req, res, next) {
     let url   = (req.query.url || '').toLowerCase();
-    let valid = validUrl.isWebUri(url);
+    let valid = false;
+    let urlObj;
 
-    if (!valid && url &&  url.substr(0, 4) !== 'http') {
-      url = 'http://' + url;
-      valid = validUrl.isWebUri(url);
+    if (url) {
+      // If no protocol exist, default to http.
+      url = ensureProtocol(url);
+
+      try {
+        urlObj = new URL(url);
+      } catch (e) {}
+
+      if (urlObj) {
+        url   = urlObj.origin;
+        valid = true;
+
+        if (urlObj.search || urlObj.hash || urlObj.pathname.length > 1) {
+          return res.redirect('/?url=' + urlObj.origin);
+        }
+      }
     }
 
     req.context = req.context || {};
