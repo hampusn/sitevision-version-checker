1fc9aec9fe0a9a689c1c80b1201dd157418a5b8b
Hampus Nordin <nordin.hampus@gmail.com>
2017-12-04 08:00:34 +0100

    Added license information in footer

--- middlewares/context.js
+++ middlewares/context.js
@@ -1,4 +1,24 @@
 // Add view data to request
+const URL = require('url').URL;
+
+function removeTrailingSlashes (str) {
+  return str.replace(/\/+$/, '');
+}
+
+function removeLeadingSlashes (str) {
+  return str.replace(/^\/+/, '');
+}
+
+function githubUrl (url, path) {
+  let urlObj = new URL(url);
+
+  urlObj.search   = '';
+  urlObj.hash     = '';
+  urlObj.pathname = removeTrailingSlashes(urlObj.pathname) + '/' + removeLeadingSlashes(path);
+
+  return urlObj.toString();
+}
+
 module.exports = () => {
   const project = require('../package');
 
@@ -10,7 +30,9 @@ module.exports = () => {
       "author": project.author,
       "license": project.license,
       "name": project.name,
-      "githubUrl": project.homepage
+      "githubUrl": project.homepage,
+      "licenseSPDX": project.license,
+      "licenseUrl": githubUrl(project.homepage, 'blob/master/LICENSE')
     };
 
     next();
